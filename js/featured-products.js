4b99d98f5ae008efe47bd594e93a5654520ca859
Daniel Maciel Acevedo <anndannma@outlook.com>
2022-11-29 03:41:03 -0600

    Fix, se solucionó el color inicial de featured-product

--- js/featured-products.js
+++ js/featured-products.js
@@ -1,6 +1,5 @@
 const selectButton = document.querySelector('.select1');
 const productContent = document.querySelector('.product1');
-let firstProduct = false;
 
 const selectButton2 = document.querySelector('.select2');
 const productContent2 = document.querySelector('.product2');
@@ -8,12 +7,23 @@ const productContent2 = document.querySelector('.product2');
 const selectButton3 = document.querySelector('.select3');
 const productContent3 = document.querySelector('.product3');
 
+productContent2.classList.add('none');
+productContent3.classList.add('none');
+
+let firstProduct = false;
+
 selectButton.addEventListener('click', () => 
 {
-        productContent.classList.remove('none');
-        productContent2.classList.add('none');
-        productContent3.classList.add('none');
-        firstProduct = true;
+        if (!firstProduct) {
+                productContent.classList.remove('none');
+                productContent2.classList.add('none');
+                productContent3.classList.add('none');
+                firstProduct = true;
+        } else {
+                productContent2.classList.add('none');
+                productContent3.classList.add('none');
+                firstProduct = false;
+        }
 });
 
 selectButton2.addEventListener('click', () => 
