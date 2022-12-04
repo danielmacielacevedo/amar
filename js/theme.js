7182050b3d2d94965b9ef4b170c8d564c903971f
Daniel Maciel Acevedo <anndannma@outlook.com>
2022-12-04 01:57:33 -0600

    Fix, ahora se guarda la configuración del usuario

--- js/theme.js
+++ js/theme.js
@@ -1,21 +1,55 @@
 const themeButton = document.querySelector('.theme-selector');
 const themeIcon = document.querySelector('.theme-img');
-let themeOpen = false;
 
-themeButton.addEventListener('click', () => 
+
+if(localStorage.getItem('theme') == null)
 {
-    if(!themeOpen) 
-    {
+    localStorage.setItem('theme', 'light');
+}
+
+let localData = localStorage.getItem('theme');
+
+if(localData == 'dark'){
+    document.body.classList.add('theme');
+    themeButton.classList.add('open');
+    themeIcon.classList.add('open');
+} else if(localData == 'light'){
+    themeButton.classList.remove('open');
+    themeIcon.classList.remove('open');
+    document.body.classList.remove('theme');
+}
+
+themeButton.onclick = function(){
+    document.body.classList.toggle('theme');
+    if(document.body.classList.contains('theme')){
         themeButton.classList.add('open');
         themeIcon.classList.add('open');
-        themeOpen = true;
+        localStorage.setItem('theme', 'dark');
     } else {
         themeButton.classList.remove('open');
         themeIcon.classList.remove('open');
-        themeOpen = false;
+        localStorage.setItem('theme', 'light');
     }
-});
-
-themeButton.onclick = function() {
-    document.body.classList.toggle('theme');
 }
+
+// const themeButton = document.querySelector('.theme-selector');
+// const themeIcon = document.querySelector('.theme-img');
+// let themeOpen = false;
+
+// themeButton.addEventListener('click', () => 
+// {
+//     if(!themeOpen) 
+//     {
+//         themeButton.classList.add('open');
+//         themeIcon.classList.add('open');
+//         themeOpen = true;
+//     } else {
+//         themeButton.classList.remove('open');
+//         themeIcon.classList.remove('open');
+//         themeOpen = false;
+//     }
+// });
+
+// themeButton.onclick = function() {
+//     document.body.classList.toggle('theme');
+// }
