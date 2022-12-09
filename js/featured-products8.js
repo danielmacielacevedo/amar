17a1bf13961d23e9c08f30fbe93c20e7e6cdd1c8
Daniel Maciel Acevedo <anndannma@outlook.com>
2022-12-09 01:30:49 -0600

    Beta 1.1 | Se agregó el primer producto oficial

---
+++ js/featured-products8.js
@@ -0,0 +1,149 @@
+const selectButton = document.querySelector('.select1');
+const productContent = document.querySelector('.product1');
+
+const selectButton2 = document.querySelector('.select2');
+const productContent2 = document.querySelector('.product2');
+
+const selectButton3 = document.querySelector('.select3');
+const productContent3 = document.querySelector('.product3');
+
+const selectButton4 = document.querySelector('.select4');
+const productContent4 = document.querySelector('.product4');
+
+const selectButton5 = document.querySelector('.select5');
+const productContent5 = document.querySelector('.product5');
+
+const selectButton6 = document.querySelector('.select6');
+const productContent6 = document.querySelector('.product6');
+
+const selectButton7 = document.querySelector('.select7');
+const productContent7 = document.querySelector('.product7');
+
+const selectButton8 = document.querySelector('.select8');
+const productContent8 = document.querySelector('.product8');
+
+productContent2.classList.add('none');
+productContent3.classList.add('none');
+productContent4.classList.add('none');
+productContent5.classList.add('none');
+productContent6.classList.add('none');
+productContent7.classList.add('none');
+productContent8.classList.add('none');
+
+let firstProduct = false;
+
+selectButton.addEventListener('click', () => 
+{
+        if (!firstProduct) {
+                productContent.classList.remove('none');
+                productContent2.classList.add('none');
+                productContent3.classList.add('none');
+                productContent4.classList.add('none');
+                productContent5.classList.add('none');
+                productContent6.classList.add('none');
+                productContent7.classList.add('none');
+                productContent8.classList.add('none');
+                firstProduct = true;
+        } else {
+                productContent2.classList.add('none');
+                productContent3.classList.add('none');
+                productContent4.classList.add('none');
+                productContent5.classList.add('none');
+                productContent6.classList.add('none');
+                productContent7.classList.add('none');
+                productContent8.classList.add('none');
+                firstProduct = false;
+        }
+});
+
+selectButton2.addEventListener('click', () => 
+{
+        productContent.classList.add('none');
+        productContent2.classList.remove('none');
+        productContent3.classList.add('none');
+        productContent4.classList.add('none');
+        productContent5.classList.add('none');
+        productContent6.classList.add('none');
+        productContent7.classList.add('none');
+        productContent8.classList.add('none');
+        firstProduct = false;
+});
+
+selectButton3.addEventListener('click', () => 
+{
+        productContent.classList.add('none');
+        productContent2.classList.add('none');
+        productContent3.classList.remove('none');
+        productContent4.classList.add('none');
+        productContent5.classList.add('none');
+        productContent6.classList.add('none');
+        productContent7.classList.add('none');
+        productContent8.classList.add('none');
+        firstProduct = false;
+});
+
+selectButton4.addEventListener('click', () => 
+{
+        productContent.classList.add('none');
+        productContent2.classList.add('none');
+        productContent3.classList.add('none');
+        productContent4.classList.remove('none');
+        productContent5.classList.add('none');
+        productContent6.classList.add('none');
+        productContent7.classList.add('none');
+        productContent8.classList.add('none');
+        firstProduct = false;
+});
+
+selectButton5.addEventListener('click', () => 
+{
+        productContent.classList.add('none');
+        productContent2.classList.add('none');
+        productContent3.classList.add('none');
+        productContent4.classList.add('none');
+        productContent5.classList.remove('none');
+        productContent6.classList.add('none');
+        productContent7.classList.add('none');
+        productContent8.classList.add('none');
+        firstProduct = false;
+});
+
+selectButton6.addEventListener('click', () => 
+{
+        productContent.classList.add('none');
+        productContent2.classList.add('none');
+        productContent3.classList.add('none');
+        productContent4.classList.add('none');
+        productContent5.classList.add('none');
+        productContent6.classList.remove('none');
+        productContent7.classList.add('none');
+        productContent8.classList.add('none');
+        firstProduct = false;
+});
+
+selectButton7.addEventListener('click', () => 
+{
+        productContent.classList.add('none');
+        productContent2.classList.add('none');
+        productContent3.classList.add('none');
+        productContent4.classList.add('none');
+        productContent5.classList.add('none');
+        productContent6.classList.add('none');
+        productContent7.classList.remove('none');
+        productContent8.classList.add('none');
+        firstProduct = false;
+});
+
+selectButton8.addEventListener('click', () => 
+{
+        productContent.classList.add('none');
+        productContent2.classList.add('none');
+        productContent3.classList.add('none');
+        productContent4.classList.add('none');
+        productContent5.classList.add('none');
+        productContent6.classList.add('none');
+        productContent7.classList.add('none');
+        productContent8.classList.remove('none');
+        firstProduct = false;
+});
+
